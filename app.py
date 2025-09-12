cb86abbb32b1c3cb453de9263c152ab918e7b48f
c6rg0 <shinylaunchpad778@gmail.com>
2025-09-12 14:37:55 +0100

    Still got the same error

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/hello") # "/hello" routes to "http://url.com/hello"
-def hello_world():
-    name = request.args.get("name", "Flask")
-    return f"Hello, {escape(name)}!"
-
 
