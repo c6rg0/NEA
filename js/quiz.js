3530972f2ef507af417f4b97bc37a1a60083060a
c6rg0 <shinylaunchpad778@gmail.com>
2025-09-16 10:56:16 +0100

    Fixed bugs, learning to use DOM

--- js/quiz.js
+++ js/quiz.js
@@ -8,9 +8,26 @@ quiz_meta.set("Length", 5);
 const quiz_q = [];
 quiz_q.push("", "", "", "", "");
 
-
 const quiz_a = [];
 quiz_a.push("", "", "", "", "");
+
+/* Pre-quiz screen */
+
+document.querySelector("input").addEventListener("click", () =>
+
+ {
+	 const header = document.getElementsByTagName("h1").item(0);
+	 header.firstChild.data = "A dynamic document";
+
+	const question_text = document.createTextNode("Where the qestion is going to go");
+
+	const newElement = document.createElement("h1");
+	newElement.appendChild(question_text);
+	para.parentNode.appendChild(newElement);
+
+/* Quiz */
+
+/* Initlialize quiz visual objects */
 
 let range = quiz_meta("Length");
 range = range - 1;
@@ -20,7 +37,31 @@ for (let i = 0; i < range; i++) {
 	/* Display quiz_q[i]
 	 * Test the answer
 	 * Display condition
-	 * Effect
+	 * Effect - score
 	 * End */
 }
+
+/* Structure:
+ *	Displaying a "Play" screen (before the quiz),
+ *	Removing the existing objects,
+ *	Adding the buttons,
+ * 	Adding text for the quiz,
+ * 	Waiting for the answer,
+ * 	Checking for incorrect/correct answer,
+ * 	Keeping track of the score
+ * 	Displaying so,
+ * 	Next question (looping),
+ * 	When the quiz is done:
+ * 		Removing all the existing objects
+ * 		Adding a quiz screen (with scores)
+ *
+
+
+
+
+
+
+
+
+
 
