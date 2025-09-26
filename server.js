ed998f0860ee8d48035c3bc10b2c41db89d55572
c6rg0 <shinylaunchpad778@gmail.com>
2025-09-26 12:51:23 +0000

    Set up(?) sqlite3 in server.js (not tested)

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const sqlite3 = require('sqlite');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
 
-let db = new sqlite3.Database(":memory:", (err) => {
-	if(err){
-		console.log("Error occured -" + err.message);
-	} else {
-		console.log("Database connected");
+db.serialize(() => {
+	db.run("CREATE TABLE lorem (info TEXT)");
+
+	const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+	for (let i = 0; i < 10; i++) {
+		stmt.run("Ipsum " + i);
 	}
-})
+	stmt.finalize();
+
+	db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(row.id + ": " + row.info);
+	});
+});
+
+db.close();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
