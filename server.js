b826e8ab80d7d6a9fb244e9f495a08e89622113d
c6rg0 <shinylaunchpad778@gmail.com>
2025-10-09 13:46:14 +0000

    Swapped postgresql with better-sqlite3, thinking of making an electron app instead of a website, which would suit sqlite more

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const Pool = require('pg').Pool;
-// const cors = require('cors');
 
-const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'data',
-	password: 'postgres',
-	dialect: 'postgres',
-	port: 5432
-});
+const Database = require('better-sqlite3');
+const quiz_db = new Database('quiz.db', { verbose: console.log });
+const account_db = new Database('account.db', { verbose: console.log });
 
-pool.connect((err, client, release) => {
-	if (err) {
-		return console.error(
-			'Error aquiring client', err.stack)
-	}
-	client.query('SELECT NOW()', (err, result) => {
-		release()
-		if (err) {
-			return console.error(
-				'Error excecuting query', err.stack)
-		}
-		console.log("Connected to database!")
-	})
-})
+quiz_db.pragma('journal_mode = WAL')
+account_db.pragma('journal_mode = WAL')
 
-//app.use(cors());
-//app.use(bodyParser.json());
+quiz_db.exec(`
+	CREATE TABLE IF NOT EXISTS Meta(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		[key] TEXT UNIQUE,
+		[value] TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Settings(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT UNIQUE,
+		value TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Questions(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_text TEXT,
+		category TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Options(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_id INTEGER,
+		option_text TEXT,
+		FOREIGN KEY (question_id) REFERENCES Questions(id)
+	);
+
+	CREATE TABLE IF NOT EXISTS Answers(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_id INTEGER,
+		option_id INTEGER,
+		FOREIGN KEY (question_id) REFERENCES Questions(id),
+		FOREIGN KEY (option_id) REFERENCES Options(id)
+		
+	);
+`);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -86,3 +100,5 @@ const PORT = 8000;
 app.listen(PORT, () => {
 	console.log("Server's started at http://localhost:8000");
 })
+
+quiz_db.close();
