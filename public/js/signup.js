7b25736c6f3314c036d2b7114bfdcefbcd4a67e0
c6rg0 <shinylaunchpad778@gmail.com>
2025-10-12 00:12:47 +0000

    Successfully migrated play.js to typescript, server.js nearly migrated, other bugs which would have occured via javascript are patched.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,14 @@
 Username
-Password
 
 Password_1
 Password_confirm
+
+// The username has to be between 6 and 20 characters.
+//
+// The password has to have upercase and lowercase, a number, 
+// and a symbol.
+
+
+
 
 
---
+++ server.ts
@@ -0,0 +1,122 @@
+import express = require('express');
+const app = express();
+import path = require('path');
+
+import Database = require('better-sqlite3');
+const quiz_db = new Database('quiz.db', { verbose: console.log });
+const account_db = new Database('account.db', { verbose: console.log });
+
+quiz_db.pragma('journal_mode = WAL');
+account_db.pragma('journal_mode = WAL');
+
+// The code below doesn't work:
+quiz_db.exec(`
+	CREATE TABLE IF NOT EXISTS Meta(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		[key] TEXT UNIQUE,
+		[value] TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Settings(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT UNIQUE,
+		value TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Questions(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_text TEXT,
+		category TEXT
+	);
+
+	CREATE TABLE IF NOT EXISTS Options(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_id INTEGER,
+		option_text TEXT,
+		FOREIGN KEY (question_id) REFERENCES Questions(id)
+	);
+
+	CREATE TABLE IF NOT EXISTS Answers(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		question_id INTEGER,
+		option_id INTEGER,
+		FOREIGN KEY (question_id) REFERENCES Questions(id),
+		FOREIGN KEY (option_id) REFERENCES Options(id)
+		
+	);
+`);
+
+
+account_db.exec(`
+	CREATE TABLE IF NOT EXISTS Logins(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		[username] TEXT UNIQUE,
+		[password] TEXT
+	);
+`);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setting up the different parts of my website
+// Example: "/play" -> "http://localhost:8000/play"
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+app.get("/browse", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'browse.html'));
+})
+
+app.get("/play", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'play.html'));
+})
+
+app.get("/create", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'create.html'));
+})
+
+app.get("/account", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'account.html'));
+})
+
+app.get("/signup", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+})
+
+
+app.get("/signup", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+})
+
+app.get("/signup-success", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'signup-success.html'));
+})
+
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'login.html'));
+})
+
+// Login post (to compare and validate account/passwords)
+
+// Signup check (to make sure that unique logins are being
+// created)
+
+// Signup post (to insert the new logins into the database
+
+//app.post('/submit-quiz-metadata', (req, res) => {
+//	const userData = req.body;
+//	console.log('Recieve data from client:', userdata);
+//
+	// SQL logic below:
+//	
+//	quiz_db.prepare('INSERT INTO table (value1, value2, value3) VALUES(?, ?, ?)');
+//	insert.run('value1', 'value2', 'value3');
+//});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+	console.log("Server's started at http://localhost:8000");
+})
+
+quiz_db.close();
